b0f692cde58d649dd8f09e3e72e3ef46114a22aa
ruby -v <exit>
2014-11-09 20:07:13 +0800

    liying first commit

---
+++ app/models/instrument.rb
@@ -0,0 +1,18 @@
+class Instrument < ActiveRecord::Base
+  validates_presence_of :name, :type_name, :image_url, :description, :price
+  validates_numericality_of :price
+  
+  # 验证输入的图片地址URL合法:是否以“.gif”、“.jpg”，或“.png"其中之一结束。
+  validates_format_of :image_url,
+  	 :multiline => true,
+  	 :with => %r{\.(gif|jpg|png)$}i,
+  	 :message => 'must be a URL for GIF, JPG or PNG image.'
+  		  
+  # 检查价格字段的值大于0
+  #validate :price_must_be_at_least_a_cent
+  #protected
+  #def price_must_be_at_least_a_cent
+	#errors.add(:price, 'should be at least 0.01' ) if price.nil？ || price < 0.01
+  #end
+ 
+end
---
+++ app/views/instruments/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @instrument, :id, :name, :type_name, :image_url, :description, :price, :created_at, :updated_at
