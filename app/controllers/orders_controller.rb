40f50fb04539ca967c51e78c094fe658973f6bdc
gcy <15562106257@163.com>
2014-12-09 16:40:49 +0800

    修改订单提交时的状态

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -43,7 +43,7 @@ class OrdersController < ApplicationController
   # POST /orders
   # POST /orders.json
   def create
-		@order = Order.new
+		@order = Order.new(:create_time=>Time.now,:state_value=>1)
 		userid = params[:order][:user_id]
 		@order.user_id = userid
 		@order.add_line_items_from_cart(@cart)
